aed66da12ee66579efb5c0ec35ab8661e9628841
ArthurLorphelin <arthur.lorphelin@yahoo.fr>
2025-01-08 16:40:24 +0100

    Création du test unitaire pour les flacons d'insuline avec use_before entre 0 et 7

--- SolutionPostReview/main.py
+++ SolutionPostReview/main.py
@@ -14,6 +14,7 @@ class MyTestCase(unittest.TestCase):
             Drug("Insulin vial", 30, 49),
             Drug("Insulin vial", 18, 37),
             Drug("Insulin vial", 7, 78),
+            Drug("Insulin vial", 4, 21),
             Drug("ARN Vaccine", 3, 6)
         ]
         self.inventory = Inventory(self.items)
@@ -81,9 +82,16 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(insulin_vial.use_before, 6)
         self.assertEqual(insulin_vial.efficiency, 75)
 
+    def test_insulin_vial_between_0_and_7_days_use_before(self):
+        self.inventory.update_efficiency()
+        insulin_vial = self.items[9]
+        self.assertEqual(insulin_vial.name, "Insulin vial")
+        self.assertEqual(insulin_vial.use_before, 3)
+        self.assertEqual(insulin_vial.efficiency, 18)
+
     def test_arn_vaccine(self):
         self.inventory.update_efficiency()
-        arn_vaccine = self.items[9]
+        arn_vaccine = self.items[10]
         self.assertEqual(arn_vaccine.name, "ARN Vaccine")
         self.assertEqual(arn_vaccine.use_before, 2)
         self.assertEqual(arn_vaccine.efficiency, 4)
