c1c20d87a72a0420f881779e14bb707159b8b658
ArthurLorphelin <arthur.lorphelin@yahoo.fr>
2025-01-08 16:02:47 +0100

    Création du Switch Case pour séparer les différents cas selon les types de médicaments Création des deux méthodes permettant de mettre à jour le use_before et l'efficiency pour les médicaments normaux

--- SolutionPostReview/hokla.py
+++ SolutionPostReview/hokla.py
@@ -1,44 +1,32 @@
 class Inventory(object):
-  def __init__(self, drugs):
-    self.drugs = drugs
+    def __init__(self, drugs):
+        self.drugs = drugs
 
-  def update_efficiency(self):
-    for drug in self.drugs:
-      if drug.name != "Old bottle of wine":
-        if drug.efficiency > 0:
-          if drug.name != "Granny recipe":
-            if drug.name == "Insulin vial":
-              drug.efficiency = drug.efficiency - 1
-              if drug.use_before < 31:
-                if drug.efficiency > 0:
-                  drug.efficiency = drug.efficiency - 1
-              if drug.use_before < 8:
-                 if drug.efficiency > 0:
-                   drug.efficiency = drug.efficiency - 1
-            else:
-              drug.efficiency = drug.efficiency - 1
-      else:
-        if drug.efficiency < 100:
-          drug.efficiency = drug.efficiency + 1
-      if drug.name != "Granny recipe":
-        drug.use_before = drug.use_before - 1
-      if drug.use_before < 0:
-        if drug.name != "Old bottle of wine":
-          if drug.name != "Insulin vial":
-            if drug.efficiency > 0:
-              if drug.name != "Granny recipe":
-                drug.efficiency = drug.efficiency - 1
-          else:
-            drug.efficiency = drug.efficiency - drug.efficiency
+    def update_efficiency(self):
+        for drug in self.drugs:
+            match drug.name:
+                case "Normal Drug":
+                    self.update_normal_drug_use_before(drug)
+                    self.update_normal_drug_efficiency(drug)
+
+
+
+
+    def update_normal_drug_use_before(self, drug):
+        drug.use_before -= 1
+
+    def update_normal_drug_efficiency(self, drug):
+        if drug.use_before >= 0:
+            drug.efficiency = max(drug.efficiency - 1, 0)
         else:
-          if drug.efficiency < 100:
-            drug.efficiency = drug.efficiency + 1
+            drug.efficiency = max(drug.efficiency - 2, 0)
+
 
 class Drug:
-  def __init__(self, name, use_before, efficiency):
-    self.name = name
-    self.use_before = use_before
-    self.efficiency = efficiency
+    def __init__(self, name, use_before, efficiency):
+        self.name = name
+        self.use_before = use_before
+        self.efficiency = efficiency
 
-  def __repr__(self):
-    return "%s, %s, %s" % (self.name, self.use_before, self.efficiency)
+    def __repr__(self):
+        return "%s, %s, %s" % (self.name, self.use_before, self.efficiency)
