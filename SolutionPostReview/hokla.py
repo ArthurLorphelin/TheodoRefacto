6854bbc67a24227afd1161e3fd283ed73fed7877
ArthurLorphelin <arthur.lorphelin@yahoo.fr>
2025-01-08 16:06:10 +0100

    Création des deux méthodes permettant de mettre à jour le use_before et l'efficiency pour les recettes de grand-mères

--- SolutionPostReview/hokla.py
+++ SolutionPostReview/hokla.py
@@ -11,6 +11,9 @@ class Inventory(object):
                 case "Old bottle of wine":
                     self.update_old_bottle_of_wine_use_before(drug)
                     self.update_old_bottle_of_wine_efficiency(drug)
+                case "Granny Recipe":
+                    self.update_granny_recipe_use_before()
+                    self.update_granny_recipe_efficiency()
 
 
     def update_normal_drug_use_before(self, drug):
@@ -27,6 +30,12 @@ class Inventory(object):
 
     def update_old_bottle_of_wine_efficiency(self, drug):
         drug.efficiency += 1
+
+    def update_granny_recipe_use_before(self):
+        pass
+
+    def update_granny_recipe_efficiency(self):
+        pass
 
 
 class Drug:
